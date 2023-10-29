74a9dffcad291752acfcd49ad38c17fc9ea312c9
karam <karamkhalil220@gmail.com>
2023-10-29 09:16:47 +0100

    Fixed handler for the request GET /api/getTrains

--- src/main/java/be/kuleuven/distributedsystems/cloud/controller/apiController.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/controller/apiController.java
@@ -3,25 +3,21 @@ package be.kuleuven.distributedsystems.cloud.controller;
 
 import be.kuleuven.distributedsystems.cloud.entities.Train;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 @Controller
 public class apiController {
     public final WebClient.Builder webClientBuilder;
+
 
     @Autowired
     public apiController(WebClient.Builder webClientBuilder){
@@ -29,7 +25,7 @@ public class apiController {
     }
 
     @GetMapping("/api/getTrains")
-    public ResponseEntity<?> getTrains(){
+    public ResponseEntity<Train[]> getTrains(){
         WebClient client = webClientBuilder
                 .baseUrl("https://reliabletrains.com")
                 .build();
@@ -43,8 +39,11 @@ public class apiController {
         ObjectMapper mapper = new ObjectMapper();
 
         try {
-            JsonNode node = mapper.readTree(resp);
-            return ResponseEntity.ok(node.get("_embedded"));
+            JsonNode node = mapper.readTree(resp).get("_embedded").get("trains");
+            Train[] trains = mapper.treeToValue(node, Train[].class);
+
+            return ResponseEntity.ok(trains);
+
         } catch (JsonProcessingException e) {
             System.out.println("Error: " + e.getMessage());
         }
